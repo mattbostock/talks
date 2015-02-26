0a258c9bf0c7634643db44b1d1fa3e60a59d3460
Matt Bostock <matt@mattbostock.com>
2015-02-26 17:19:08 +0000

    Add h1 tags to make the example show larger

--- 2015-02-26-go_london_building_web_apps/code/http-handlerfunc.go
+++ 2015-02-26-go_london_building_web_apps/code/http-handlerfunc.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	hi := func(w http.ResponseWriter, r *http.Request) { // HL
-		w.Write([]byte("Hello, world.")) // HL
+		w.Write([]byte("<h1>Hello, world.</h1>")) // HL
 	} // HL
 
 	log.Fatal(http.ListenAndServe(":8080", http.HandlerFunc(hi)))
